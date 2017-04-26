4bc1e08235b36a6547b937115255fc3113f140ab
James Roper <james@jazzy.id.au>
2017-04-26 21:36:18 +1000

    Added broker example (#11)
    * Added broker example
    
    * Restructured and added travis build

---
+++ src/main/g8/$name__norm$-stream-api/src/main/java/$package$stream/api/$name__Camel$StreamService.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
+ */
+package $package$stream.api;
+
+import static com.lightbend.lagom.javadsl.api.Service.named;
+import static com.lightbend.lagom.javadsl.api.Service.namedCall;
+
+import akka.NotUsed;
+import akka.stream.javadsl.Source;
+import com.lightbend.lagom.javadsl.api.Descriptor;
+import com.lightbend.lagom.javadsl.api.Service;
+import com.lightbend.lagom.javadsl.api.ServiceCall;
+
+/**
+ * The $name$ stream interface.
+ * <p>
+ * This describes everything that Lagom needs to know about how to serve and
+ * consume the $name;format="Camel"$StreamService service.
+ */
+public interface $name;format="Camel"$StreamService extends Service {
+
+  /**
+   * This stream is implemented by asking the hello service directly to say
+   * hello to each passed in name. It requires the hello service to be up
+   * and running to function.
+   */
+  ServiceCall<Source<String, NotUsed>, Source<String, NotUsed>> directStream();
+
+  /**
+   * This stream is implemented autonomously, it uses its own store, populated
+   * by subscribing to the events published by the hello service, to say hello
+   * to each passed in name. It can function even when the hello service is
+   * down.
+   */
+  ServiceCall<Source<String, NotUsed>, Source<String, NotUsed>> autonomousStream();
+
+  @Override
+  default Descriptor descriptor() {
+    return named("$name;format="norm"$-stream")
+        .withCalls(
+            namedCall("direct-stream", this::directStream),
+            namedCall("auto-stream", this::autonomousStream)
+        ).withAutoAcl(true);
+  }
+}
---
+++ src/main/g8/$name__norm$-stream-impl/src/main/java/$package$stream/impl/$name__Camel$StreamModule.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
+ */
+package $package$stream.impl;
+
+import com.google.inject.AbstractModule;
+import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
+import $package$.api.$name;format="Camel"$Service;
+import $package$stream.api.$name;format="Camel"$StreamService;
+
+/**
+ * The module that binds the $name;format="Camel"$StreamService so that it can be served.
+ */
+public class $name;format="Camel"$StreamModule extends AbstractModule implements ServiceGuiceSupport {
+  @Override
+  protected void configure() {
+    // Bind the $name;format="Camel"$StreamService service
+    bindServices(serviceBinding($name;format="Camel"$StreamService.class, $name;format="Camel"$StreamServiceImpl.class));
+    // Bind the $name;format="Camel"$Service client
+    bindClient($name;format="Camel"$Service.class);
+    // Bind the subscriber eagerly to ensure it starts up
+    bind($name;format="Camel"$StreamSubscriber.class).asEagerSingleton();
+  }
+}
